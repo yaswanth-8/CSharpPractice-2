760b775a182c3584b212e7ade3af02774399c31d
yaswanth <ya8wanth@gmail.com>
2023-03-23 16:35:04 +0530

    Shoting game in c#

---
+++ CSharpPractice/BulletGame.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice
+{
+    internal class BulletGame
+    {
+
+        static int heroBullets = 100;
+        static int villainBullets = 100;
+        static void heroShoot()
+        {
+            heroBullets--;
+        }
+        static void villainShoot()
+        {
+            villainBullets = villainBullets - 3;
+        }
+        static void display()
+        {
+            Console.WriteLine("hero's balance : " + heroBullets);
+            Console.WriteLine("villain's balance : " + villainBullets);
+        }
+
+        static void Main(string[] args)
+        {
+            bool next = true;
+            while (next)
+            {
+                Console.WriteLine("who want to take shot hero , villain , exit for quit :");
+                string key = Console.ReadLine();
+                if (key == "hero")
+                {
+                    heroShoot();
+                }
+                else if (key == "villain")
+                {
+                    villainShoot();
+                }
+                else if (key == "exit")
+                {
+                    next = false;
+                }
+                else
+                {
+                    Console.WriteLine("invalid");
+                }
+                display();
+                Console.WriteLine("--------------------------------------------------------------------------------------");
+            }
+        }
+
+    }
+}
--- CSharpPractice/Program.cs
+++ CSharpPractice/Program.cs
@@ -3,19 +3,4 @@
 
 class Program
 {
-
-    public static void square(out int a)
-    {
-        a = 10;
-        a = a * a;
-    }
-    
-    static void Main(string[] args)
-    {
-        int a = 2;
-        square(out a);
-        Console.WriteLine(a);
-       
-       
-    }
 }
