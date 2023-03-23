6f4f701e5f273db21d59eccebbebfd7a6cab2f97
yaswanth <ya8wanth@gmail.com>
2023-03-23 14:04:53 +0530

    exception handling

--- CSharpPractice/Program.cs
+++ CSharpPractice/Program.cs
@@ -3,6 +3,19 @@ class Program
 {
     static void Main(string[] args)
     {
-       Console.WriteLine("Thursday practice");
+        int result=0;
+        int zero = 0;
+        try
+        {
+            result = 5 / zero;
+        }
+        catch (DivideByZeroException e)
+        {
+            Console.WriteLine("Exception caught: {0}", e);
+        }
+        finally
+        {
+            Console.WriteLine("Result: {0}", result);
+        }
     }
 }
